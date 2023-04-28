f0e07bd9e0ca37320a2af0d2858c8599421382b6
malefane <malefane@vut.ac.za>
2023-04-28 14:24:47 +0200

    Added new line on the main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, DevOps students !!");
+        System.out.println("Thank you for watching !!");
     }
 }
